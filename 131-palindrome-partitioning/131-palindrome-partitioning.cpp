a43b3a9b451dab308fbdd71fe149393c4edb87ef
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-07-05 15:29:34 +0530

    Time: 207 ms (50.09%), Space: 57.6 MB (49.75%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
- bool isBool(string s, int start, int end) {
-    while (start <= end) {
-      if (s[start++] != s[end--])
-        return false;
+    bool isBool(string s, int p1,int p2)
+    {
+        while(p1<=p2)
+        {
+            if(s[p1++]!=s[p2--])
+                return false;
+        }
+        return true;
     }
-    return true;
-  }
-   void solve(int index, string s, vector < string > & path,
-    vector < vector < string > > & res) {
-    if (index == s.size()) {
-      res.push_back(path);
-      return;
-    }
-    for (int i = index; i < s.size(); ++i) {
-      if (isBool(s, index, i)) {
-        path.push_back(s.substr(index, i - index + 1));
-        solve(i + 1, s, path, res);
-        path.pop_back();
+    void solve(int ind, vector<string>&ds, vector<vector<string>>&ans, string s)
+    {
+        if(ind==s.size())
+        {
+            ans.push_back(ds);
+            return ;
+        }
+        
+    for (int i = ind; i < s.size(); ++i) {
+      if (isBool(s, ind, i)) {
+        ds.push_back(s.substr(ind, i - ind + 1)); 
+        solve(i + 1, ds, ans, s); 
+        ds.pop_back();  
       }
     }
-  }
+    }
     vector<vector<string>> partition(string s) {
         vector<string>ds;
         vector<vector<string>>ans;
-        solve(0, s, ds, ans);
+        solve(0, ds, ans, s);
         return ans;
     }
 };
+
+
+
+
+
+
+
+

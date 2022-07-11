7d8d057c954f5e2247d60fb6390026729da13ee1
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-07-12 00:08:56 +0530

    Time: 20 ms (80.67%), Space: 16.4 MB (96.62%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int i, j, maxR=nums[0], n=nums.size();
+        int jump=1;
+        int steps=nums[0];
+       
+        if(n==1){
+            return 0;
+        }
+         if(nums[0]==0)
+        return -1;
+        for(i=1; i<n-1; i++)
+        {
+            maxR= max(maxR, i+nums[i]);
+            steps--;
+            if(steps==0)
+            {
+                jump++; 
+                if(i>=maxR)
+                return -1;
+                steps = maxR-i;
+            }
+        }
+        
+    return jump;
+    }
+};
+//  int minJumps(int arr[], int n)
+//     {
+//         int i, j, maxR=arr[0];
+//         int jump=1;
+//         int steps=arr[0];
+       
+//         if(n==1){
+//             return 0;
+//         }
+//          if(arr[0]==0)
+//         return -1;
+//         for(i=1; i<n-1; i++)
+//         {
+//             maxR= max(maxR, i+arr[i]);
+//             steps--;
+//             if(steps==0)
+//             {
+//                 jump++; 
+//                 if(i>=maxR)
+//                 return -1;
+//                 steps = maxR-i;
+//             }
+//         }
+        
+//     return jump;
+// }

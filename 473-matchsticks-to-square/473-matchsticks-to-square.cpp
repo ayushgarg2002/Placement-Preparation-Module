855dd651eda436111b58ab4ec3dc4bc6e8507fed
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-07-12 21:11:21 +0530

    Time: 1004 ms (14.75%), Space: 10.2 MB (47.49%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  bool solve(int ind, vector<int>&matchsticks, vector<int>&vec, int target)
+  {
+      if(ind==matchsticks.size())
+      {
+          if(vec[0]==vec[1] && vec[1]==vec[2] && vec[2]==vec[3]){
+              return true;
+          }
+        return false;
+                                    
+      }
+      
+      for(int i=0;i<4;i++)
+      {
+          if(vec[i] + matchsticks[ind] > target)
+              continue;
+          vec[i] += matchsticks[ind];
+          if(solve(ind+1, matchsticks, vec, target))
+              return true;
+          vec[i]-=matchsticks[ind];
+      }
+      return false;
+  }
+    bool makesquare(vector<int>& matchsticks) {
+     vector<int> vec(4, 0) ;
+        int n= matchsticks.size(), i,j, sum=0;
+        for(i=0;i<n;i++)
+            sum+= matchsticks[i];
+        if(sum==0 || sum%4!=0)
+            return false;
+        int target = sum/4;
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>());
+        if(solve(0, matchsticks, vec, target))
+            return true;
+        return false;
+        
+    }
+};

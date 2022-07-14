0f21a2c1f8889a3d82486a4bc660255427dddad2
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-07-15 00:53:50 +0530

    Time: 46 ms (32.08%), Space: 26.5 MB (22.23%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -37,34 +37,3 @@ public:
         return root;
     }
 };
-// class Solution {
-// public:
-//     TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int &postStart, int inStart, int inEnd, int n, map<int, int> &mapi)
-//     {
-      
-//         if(postStart< 0 || inStart>inEnd)
-//             return NULL;
-        
-//         int ind = postorder[postStart--];
-//         int partition  = mapi[ind];
-        
-//         TreeNode* root = new TreeNode(ind);
-//         root->right= solve(inorder, postorder, postStart, partition+1, inEnd, n, mapi) ;
-//         root->left= solve( inorder,postorder, postStart, inStart, partition-1, n , mapi);
-        
-        
-//         return root;
-//     }
-        
-//       TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-//      int n= postorder.size();
-//      int postInd = n-1;
-//      map<int, int> mapi;
-//         for(int i=0; i<n; i++)
-//         {
-//             mapi[inorder[i]] = i;
-//         }
-//         TreeNode* root = solve(inorder, postorder, postInd, 0, n-1, n, mapi);
-//         return root;
-//     }
-// };

e52922da1fe06c44c2416b38f889cedcfec35359
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-07-16 14:37:04 +0530

    Time: 19 ms (90.87%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool isvalid(int i, int j, vector<vector<int>>& grid, int n, int m)
+    // bool isvalid(int i, int j, vector<vector<int>>& grid, int n, int m)
+    // {
+    //     if(i<n && j<m && i>=0 && j>=0 && grid[i][j]==1)
+    //         return  true;
+    //     return false;
+    // }
+    int  solve(int i, int j, vector<vector<int>>& grid, int n, int m, int &ans)
     {
-        if(i<n && j<m && i>=0 && j>=0 && grid[i][j]==1)
-            return  true;
-        return false;
-    }
-    void solve(int i, int j, vector<vector<int>>& grid, int n, int m, int &ans)
-    {
-        // if(grid[i][j]==0)
-        //     return;
-        // if(i>=n || j>=n || i<0 || j<0)
-        //     return;
+        if(i>=n || j>=m || i<0 || j<0) 
+            return 0;
+        if(grid[i][j]==0)
+            return 0;
         ans++;
         grid[i][j]=0;
-        cout<<"cat"<<" ";
-        if(isvalid(i+1, j, grid, n, m))
-            
-        solve(i+1, j, grid, n, m, ans);
-        if(isvalid(i-1, j, grid, n, m))
-        solve(i-1, j, grid, n, m, ans);
-        if(isvalid(i, j+1, grid, n, m))
-        solve(i, j+1, grid, n, m, ans);
-        if(isvalid(i, j-1, grid, n, m))
-        solve(i, j-1, grid, n, m, ans);
+      
+        // if(isvalid(i+1, j, grid, n, m))
+        // solve(i+1, j, grid, n, m, ans);
+        // if(isvalid(i-1, j, grid, n, m))
+        // solve(i-1, j, grid, n, m, ans);
+        // if(isvalid(i, j+1, grid, n, m))
+        // solve(i, j+1, grid, n, m, ans);
+        // if(isvalid(i, j-1, grid, n, m))
+        // solve(i, j-1, grid, n, m, ans);
         
+        return 1+ solve(i+1, j, grid, n, m, ans)+solve(i-1, j, grid, n, m, ans)+  solve(i, j+1, grid, n, m, ans)+  solve(i, j-1, grid, n, m, ans);
     }
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
@@ -38,8 +38,8 @@ public:
         {
             if(grid[i][j]==1){
                 int ans=0;
-                 solve(i,j, grid, n,m, ans);
-                res = max(res, ans);
+                 int ok= solve(i,j, grid, n,m, ans);
+                res = max(res, ok);
             }
         }
      }

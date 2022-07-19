5876d2cd2c90dbf8909639bd9ea93df8c8226bf7
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-07-19 18:16:32 +0530

    Time: 14 ms (59.99%), Space: 14.3 MB (15.80%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     
-   void dfs(vector<vector<int>>& vis ,vector<vector<int>>& image, int sr, int sc, int x, int y, int old, int newColor)
+    void solve(vector<vector<int>>& image, int sr, int sc, vector<vector<int>> &vis, int newColor, int x, int y, int oldColor)
     {
-        if(sr<0 || sc<0 || sr>=x || sc>=y)
+        if(sr >=x || sc>= y | sr<0 || sc<0)
             return;
-        
-        if(vis[sr][sc]==1 || image[sr][sc]!= old)
+        if(image[sr][sc]!=oldColor )
             return;
-        image[sr][sc]= newColor;
-        vis[sr][sc]=1;
-        dfs(vis,image, sr, sc+1, x,y, old, newColor);
-        dfs(vis,image, sr, sc-1, x,y,old, newColor);
-        dfs(vis,image, sr+1, sc, x,y,old, newColor);
-        dfs(vis,image, sr-1, sc, x,y,old, newColor);
-        
-        //vis[sr][sc]=0;    yaha ye ni krange kyuki yaha hme sare possible path ni dhekne bs ek bbar value change krni h image ki
-        
+        if(vis[sr][sc]) 
+            return;
+        image[sr][sc]=newColor;
+      vis[sr][sc]=1;
+           
+        solve(image, sr+1, sc, vis, newColor, x,y,oldColor);
+        solve(image, sr, sc+1, vis, newColor, x,y,oldColor);
+        solve(image, sr-1, sc, vis, newColor, x,y,oldColor);
+        solve(image, sr, sc-1, vis, newColor, x,y,oldColor);
     }
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        
-        int x= image.size();
-        int y= image[0].size();
+        int x = image.size();
+        int y = image[0].size();
+        int oldColor = image[sr][sc];
         vector<vector<int>> vis(x, vector<int>(y,0));
-        int old= image[sr][sc];
-        dfs(vis, image, sr, sc,x,y,old, newColor);
+        solve(image, sr, sc, vis, newColor,x , y, oldColor);
         return image;
     }
 };

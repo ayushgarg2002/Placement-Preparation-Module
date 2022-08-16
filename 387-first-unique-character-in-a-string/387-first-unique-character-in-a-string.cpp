dc5e9d57bb08f5cc1bef25179a72b00bb656e398
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-08-16 15:15:54 +0530

    Time: 62 ms (46.32%), Space: 10.7 MB (71.90%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> mpp;
+        
+        unordered_map<char,int> ump(26);
         for(int i=0; i<s.size(); i++){
-            mpp[s[i]-'a']++;
+            ump[s[i]]++;
         }
-        for(int i=0; i<s.size(); i++){
-            if(mpp[s[i]-'a'] == 1){
-                return i;
-            }
+           
+        
+         for(int i=0; i<s.size(); i++){
+           if(ump[s[i]]==1) return i;
         }
+         
+        
         return -1;
     }
 };
-
-
-// class Solution {
-// public:
-//     int firstUniqChar(string s) {
-//         map<char, int> mpp;
-//         for(int i=0; i<s.size(); i++){
-//             mpp[s[i]-'a']++;
-//         }
-//        int cnt=0;
-//          map<char, int>:: iterator it;
-//         int k=0;
-//          for(it= mpp.begin(); it!=mpp.end(); it++)
-//          {
-//              if(mpp[s[k]-'a']==1)
-//                  return k;
-//              cnt++;
-//                     k++;
-//          }
-    
-       
-//         return -1;
-//     }
-// };

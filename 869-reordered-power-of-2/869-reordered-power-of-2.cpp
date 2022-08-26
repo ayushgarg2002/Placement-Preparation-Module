fd11ffa3d2e65a82e96e009058c2fdafb1ab6525
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-08-26 21:10:38 +0530

    Time: 3 ms (73.16%), Space: 6.5 MB (36.72%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        int i;
+        stringstream ss;
+        ss<<n;
+        string s, str;
+        ss>>s;
+        cout<<s;
+        sort(s.begin(), s.end());
+        vector<string> vec;
+        ss.clear();
+        
+        for(i=0; i<30; i++)
+        {
+            int k = pow(2, i);
+            ss<<k;
+            ss>>str;
+            sort(str.begin(), str.end());
+            vec.push_back(str);
+            str = "";
+            ss.clear();
+        }
+        
+        for(i=0; i<30; i++)
+        {
+            if(vec[i]==s)
+                return true;
+        }
+        return false;
+        
+    }
+    
+};

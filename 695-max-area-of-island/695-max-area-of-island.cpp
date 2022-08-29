b73fc993b5e21c10371fdfc380229c8399b3e75a
Ayush Garg <72455223+ayushgarg2002@users.noreply.github.com>
2022-08-29 22:14:07 +0530

    Time: 47 ms (13.91%), Space: 23.2 MB (61.46%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-
-    void solve(int i, int j, vector<vector<int>>& grid, int n, int m, int &ans)
+  
+    int solve(int i, int j, vector<vector<int>>&grid, int n, int m, int &cnt)
     {
-        if(i>=n || j>=m || i<0 || j<0)
-            return;
-        if(grid[i][j]==0)
-            return;
-        ans++;
-        grid[i][j]=0;
-
-        solve(i+1, j, grid, n, m, ans);
-        solve(i-1, j, grid, n, m, ans);
-        solve(i, j+1, grid, n, m, ans);
-        solve(i, j-1, grid, n, m, ans);
-        
-    }
-    
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-     int i,j,n,m;
-     n = grid.size();
-     m = grid[0].size();
-     int res=0;
-     for(i=0;i<n;i++)
-     {
-        for(j=0;j<m;j++)    
+        if(i<0 || j<0 || i>=n || j>=m)
         {
-            if(grid[i][j]==1){
-                int ans=0;
-                 solve(i,j, grid, n,m, ans);
-                res = max(res, ans);
+            return 0;
+        }
+        
+        if(grid[i][j]==0)
+        {
+            return 0;
+        }
+         cnt+=1;
+        
+        grid[i][j]=0;
+        
+       
+        solve(i+1, j, grid, n, m, cnt);
+        solve(i, j+1, grid, n, m, cnt);
+        solve(i-1, j, grid,n, m, cnt);
+        solve(i, j-1, grid, n, m, cnt);
+              
+        return cnt;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+      int i,j,n= grid.size(), m = grid[0].size(), maxi = 0;
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<m; j++)
+            {
+                if(grid[i][j]==1)
+                {
+                   int val=0;
+                    solve(i,j, grid, n,m, val);
+                    {
+                        maxi = max(maxi, val);
+                    }
+                }
             }
         }
-     }
-        return res;        
+        return maxi;
     }
 };
